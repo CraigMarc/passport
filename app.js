db0c342b9b3b737b306eef347b67e0ed4c58a115
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-12-08 13:59:24 -0500

    more lesson

--- app.js
+++ app.js
@@ -45,13 +45,32 @@ const User = mongoose.model(
       };
     })
   );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch(err) {
+      done(err);
+    };
+  });
+
   
   app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
   app.use(passport.initialize());
   app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+//app.get("/", (req, res) => res.render("index"));
+
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
+
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.post("/sign-up", async (req, res, next) => {
     try {
@@ -64,6 +83,23 @@ app.post("/sign-up", async (req, res, next) => {
     } catch(err) {
       return next(err);
     };
+  });
+
+  app.post(
+    "/log-in",
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/"
+    })
+  );
+
+  app.get("/log-out", (req, res, next) => {
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
   });
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
